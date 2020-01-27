5537e214c436f9402a7141e238a84fe8458f70bb
Manuel Romero <mrm8488@gmail.com>
2020-01-27 15:33:59 +0100

    Update autozoom.py

--- autozoom.py
+++ autozoom.py
@@ -52,16 +52,17 @@ exec(open('./models/pointcloud-inpainting.py', 'r').read())
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("--in", required=True,
-	help="path to the input image", type=str, default="./images/doublestrike.jpg")
-ap.add_argument("--out", required=True,
-	help="path to where the output should be stored", type=str, default="./autozoom.mp4")
+
 ap.add_argument("--dblShift", required=False,
 	help="dblShift", type=float, default=100.0)
 ap.add_argument("--dblZoom", required=False,
 	help="dblZoom", type=float, default=1.25)
 ap.add_argument("--dblSteps", required=False,
 	help="dblSteps", type=str, default="0.0 1.0 75")
+ap.add_argument("--in", required=True,
+	help="path to the input image", type=str, default="./images/doublestrike.jpg")
+ap.add_argument("--out", required=True,
+	help="path to where the output should be stored", type=str, default="./autozoom.mp4")
 
 
 args = vars(ap.parse_args())
